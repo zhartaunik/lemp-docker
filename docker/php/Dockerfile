e221a788589ae8080f983c96d41af40ea5e14c4f
Alex Skrashuk <askrashuk@scnsoft.com>
2022-08-10 17:23:32 +0300

    Added mysqli library. Establish the connection.

---
+++ docker/php/Dockerfile
@@ -0,0 +1,3 @@
+FROM php:8.1-fpm
+
+RUN docker-php-ext-install mysqli
--- src/index.php
+++ src/index.php
@@ -2,4 +2,10 @@
 
 declare(strict_types=1);
 
-echo "Hello World";
+//phpinfo();
+
+//echo "Hello World";
+
+$connection = mysqli_connect('mysql', 'root', 'root');
+
+$connection->query('CREATE DATABASE `lemp_docker`');
